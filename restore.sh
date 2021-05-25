6474a946e06bf7fcde595fefa4881e6c84d20eec
bleak-alpha <bleak-alpha@protonmail.ch>
2021-05-25 20:29:25 +0530

    main

--- restore.sh
+++ restore.sh
@@ -7,6 +7,7 @@ sudo cp .oh-my-zsh $"HOME"/
 sudo cp .zshrc $"HOME"/
 
 printf "Restoring Neovim and Plugins"
-sudo pacman -S neovim;
+sudo pacman -S neovim nodejs npm;
 cp .config/nvim $"HOME"/
 sudo cp .config/nvim /root/.config/
+printf "Remember to run :PlugInstall to enable all plugins"
